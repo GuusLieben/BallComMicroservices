7736c5f3cd9e3e90532566822a2588298b7053b3
Guus Lieben <guus@xendox.com>
2021-05-05 11:02:38 +0200

    Use common exchange and queue with message types

--- Shipping/src/main/java/com/ball/shipping/amqp/RabbitMQListener.java
+++ Shipping/src/main/java/com/ball/shipping/amqp/RabbitMQListener.java
@@ -1,9 +1,8 @@
 package com.ball.shipping.amqp;
 
-import com.ball.shipping.model.amqp.ShipmentDeliveredEvent;
-import com.ball.shipping.model.amqp.ShipmentReceivedEvent;
-import com.ball.shipping.model.amqp.ShipmentRegisteredEvent;
-import com.ball.shipping.model.amqp.ShipmentShippedEvent;
+import com.ball.shipping.model.amqp.Event;
+
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.QueueBinding;
@@ -15,37 +14,11 @@ public class RabbitMQListener {
 
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue(value = "${ball.rabbitmq.queue}", durable = "false"),
-            exchange = @Exchange("${ball.rabbitmq.registered}"),
+            exchange = @Exchange("${ball.rabbitmq.exchange}"),
             key = "${ball.rabbitmq.queue}"
     ))
-    public void whenShipmentRegistered(ShipmentRegisteredEvent event) {
-        System.out.println("Registered shipment for: " + event.getShipment().getFirstName() + event.getShipment().getLastName());
-    }
-
-    @RabbitListener(bindings = @QueueBinding(
-            value = @Queue(value = "${ball.rabbitmq.queue}", durable = "false"),
-            exchange = @Exchange("${ball.rabbitmq.received}"),
-            key = "${ball.rabbitmq.queue}"
-    ))
-    public void whenShipmentReceived(ShipmentReceivedEvent event) {
-        System.out.println("Received shipment for: " + event.getShipment().getFirstName() + event.getShipment().getLastName());
-    }
-
-    @RabbitListener(bindings = @QueueBinding(
-            value = @Queue(value = "${ball.rabbitmq.queue}", durable = "false"),
-            exchange = @Exchange("${ball.rabbitmq.shipped}"),
-            key = "${ball.rabbitmq.queue}"
-    ))
-    public void whenShipmentShipped(ShipmentShippedEvent event) {
-        System.out.println("Shipped shipment for: " + event.getShipment().getFirstName() + event.getShipment().getLastName());
-    }
-
-    @RabbitListener(bindings = @QueueBinding(
-            value = @Queue(value = "${ball.rabbitmq.queue}", durable = "false"),
-            exchange = @Exchange("${ball.rabbitmq.delivered}"),
-            key = "${ball.rabbitmq.queue}"
-    ))
-    public void whenShipmentDelivered(ShipmentDeliveredEvent event) {
-        System.out.println("Delivered shipment for: " + event.getShipment().getFirstName() + event.getShipment().getLastName());
+    public void whenShipmentRegistered(Message message, Event event) {
+        Object messageType = message.getMessageProperties().getHeader("MessageType");
+        System.out.println("Received event: " + messageType + " (" + event + ")");
     }
 }
--- Shipping/src/main/java/com/ball/shipping/amqp/RabbitMQSender.java
+++ Shipping/src/main/java/com/ball/shipping/amqp/RabbitMQSender.java
@@ -1,5 +1,6 @@
 package com.ball.shipping.amqp;
 
+import com.ball.shipping.model.amqp.Event;
 import com.ball.shipping.model.amqp.ShipmentDeliveredEvent;
 import com.ball.shipping.model.amqp.ShipmentReceivedEvent;
 import com.ball.shipping.model.amqp.ShipmentRegisteredEvent;
@@ -13,17 +14,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQSender {
 
+    @Value("${ball.rabbitmq.exchange}")
+    private String exchangeKey;
     @Value("${ball.rabbitmq.queue}")
     private String queueKey;
 
-    @Value("${ball.rabbitmq.registered}")
-    private String registeredKey;
-    @Value("${ball.rabbitmq.received}")
-    private String receivedKey;
-    @Value("${ball.rabbitmq.shipped}")
-    private String shippedKey;
-    @Value("${ball.rabbitmq.delivered}")
-    private String deliveredKey;
+    @Value("${ball.rabbitmq.header.registered}")
+    private String registeredHeader;
+    @Value("${ball.rabbitmq.header.received}")
+    private String receivedHeader;
+    @Value("${ball.rabbitmq.header.shipped}")
+    private String shippedHeader;
+    @Value("${ball.rabbitmq.header.delivered}")
+    private String deliveredHeader;
 
     private final RabbitTemplate template;
 
@@ -32,19 +35,25 @@ public class RabbitMQSender {
     }
 
     public void shipmentDelivered(Shipment shipment) {
-        this.template.convertAndSend(this.deliveredKey, this.queueKey, new ShipmentDeliveredEvent(shipment));
+        this.broadcast(new ShipmentDeliveredEvent(shipment), this.deliveredHeader);
     }
 
     public void shipmentReceived(Shipment shipment) {
-        this.template.convertAndSend(this.receivedKey, this.queueKey, new ShipmentReceivedEvent(shipment));
+        this.broadcast(new ShipmentReceivedEvent(shipment), this.receivedHeader);
     }
 
     public void shipmentRegistered(Shipment shipment) {
-        this.template.convertAndSend(this.registeredKey, this.queueKey, new ShipmentRegisteredEvent(shipment));
+        this.broadcast(new ShipmentRegisteredEvent(shipment), this.registeredHeader);
     }
 
     public void shipmentShipped(Shipment shipment) {
-        this.template.convertAndSend(this.shippedKey, this.queueKey, new ShipmentShippedEvent(shipment));
+        this.broadcast(new ShipmentShippedEvent(shipment), this.shippedHeader);
     }
 
+    private void broadcast(Event event, String messageType) {
+        this.template.convertAndSend(this.exchangeKey, this.queueKey, event, m -> {
+            m.getMessageProperties().setHeader("MessageType", messageType);
+            return m;
+        });
+    }
 }

f4a552bc0b638530d20b3295474bd97c55de18f5
Guus Lieben <guus@xendox.com>
2021-05-12 14:29:18 +0200

    Add role bitflags for easy role grouping

--- Authentication/src/main/java/com/ball/auth/model/UserRole.java
+++ Authentication/src/main/java/com/ball/auth/model/UserRole.java
@@ -1,5 +1,36 @@
 package com.ball.auth.model;
 
+import java.util.EnumSet;
+
 public enum UserRole {
-    CUSTOMER, SUPPLIER, EMPLOYEE
+    CUSTOMER(1<<0), SUPPLIER(1<<1), EMPLOYEE(1<<2);
+
+    private final int i;
+
+    UserRole(int i) {
+        this.i = i;
+    }
+
+    public int getI() {
+        return this.i;
+    }
+
+    public static long getStatusValue(UserRole... flags) {
+        long value=0;
+        for (UserRole flag : flags) {
+            value |= flag.i;
+        }
+        return value;
+    }
+
+    public static EnumSet<UserRole> getStatusFlags(long statusValue) {
+        EnumSet<UserRole> statusFlags = EnumSet.noneOf(UserRole.class);
+        for (UserRole flag : UserRole.values()) {
+            long flagValue = flag.i;
+            if ( (flagValue&statusValue ) == flagValue ) {
+                statusFlags.add(flag);
+            }
+        }
+        return statusFlags;
+    }
 }

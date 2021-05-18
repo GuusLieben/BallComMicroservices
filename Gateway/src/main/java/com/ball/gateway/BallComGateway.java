920468cd518e92936e09081e68dbc5c3613d2cfe
Guus Lieben <guus@xendox.com>
2021-05-18 14:23:33 +0200

    Destroy Hystrix instances on application destruction (memory leak)

--- Gateway/src/main/java/com/ball/gateway/BallComGateway.java
+++ Gateway/src/main/java/com/ball/gateway/BallComGateway.java
@@ -1,14 +1,50 @@
 package com.ball.gateway;
 
+import com.netflix.config.AggregatedConfiguration;
+import com.netflix.config.ConcurrentCompositeConfiguration;
+import com.netflix.config.ConfigurationManager;
+import com.netflix.config.DynamicConfiguration;
+
+import org.apache.commons.configuration.AbstractConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+import java.util.List;
+
+import javax.annotation.PreDestroy;
+
 @EnableZuulProxy
+@EnableCircuitBreaker
+@SpringBootApplication
 public class BallComGateway {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BallComGateway.class, args);
+	}
+
+	@PreDestroy
+	public void cleanup() {
+		try {
+			if (ConfigurationManager.getConfigInstance() instanceof DynamicConfiguration) {
+				DynamicConfiguration config = (DynamicConfiguration) ConfigurationManager.getConfigInstance();
+				config.stopLoading();
+			} else if (ConfigurationManager.getConfigInstance() instanceof ConcurrentCompositeConfiguration) {
+				AggregatedConfiguration configInst = (ConcurrentCompositeConfiguration) ConfigurationManager
+						.getConfigInstance();
+				List<AbstractConfiguration> configs = configInst.getConfigurations();
+				if (configs != null) {
+					for (AbstractConfiguration config : configs) {
+						if (config instanceof DynamicConfiguration) {
+							((DynamicConfiguration) config).stopLoading();
+							break;
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }

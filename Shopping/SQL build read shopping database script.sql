4b9c26f49e6a9ae2e23eb43cb67c9d7aaeb689a5
Melvin Mol <mmol4@avans.nl>
2021-05-24 10:04:56 +0200

    Clean sql scripts

--- Shopping/SQL build read shopping database script.sql
+++ Shopping/SQL build read shopping database script.sql
@@ -23,11 +23,3 @@ BEGIN
 		CONSTRAINT pk_basket PRIMARY KEY ([customerId])
 	);
 END;
-
-BEGIN
-	INSERT INTO product ([productId], [data])
-	VALUES ('b7ea538f-78ed-4ae7-8b7b-7f59e6f6709c', '{ "productId": "b7ea538f-78ed-4ae7-8b7b-7f59e6f6709c", "name": "Apple Watch", "amount": 43 }');
-
-INSERT INTO product ([productId], [data])
-	VALUES ('55894a2e-6063-40a3-aa08-b29b994bd750', '{ "productId": "55894a2e-6063-40a3-aa08-b29b994bd750", "name": "USB-C kabel", "amount": 43 }');
-END;
--- Shopping/SQL build write shopping databse script.sql
+++ Shopping/SQL build write shopping databse script.sql
@@ -27,20 +27,3 @@ BEGIN
 		CONSTRAINT pk_basket PRIMARY KEY ([customerId], [date])
 	);
 END;
-
-/* insert data */
-/*BEGIN
-	INSERT INTO product ([id], [date], [event], [name], [description], [price], [amount], [supplier], [brand], [details_viewed])
-	VALUES ('55894a2e-6063-40a3-aa08-b29b994bd750', '2021-05-19 21:33:41.9133333', 'created', 'USB-C kabel', 
-	'Samsung USB-C datakabel voor uw Samsung toestel met USB-C aansluiting. ' +
-	'Gebruik deze datakabel om bijvoorbeeld uw Samsung apparaat te synchroniseren met uw computer of om op te laden. ' +
-	'Deze Samsung datakabel heeft een USB-C aansluiting, duurzaam en van hoge kwaliteit.',
-	13.49, 25, 'allekabels.nl', 'Samsung', 67);
-
-	INSERT INTO product ([id], [date], [event], [name], [description], [price], [amount], [supplier], [brand], [details_viewed])
-	VALUES ('b7ea538f-78ed-4ae7-8b7b-7f59e6f6709c', '2021-05-19 21:33:41.9133333', 'created', 'AirTag', 
-	'Met de Apple AirTag hoef je nooit meer lang naar je spullen te zoeken. ' +
-	'Je kan de AirTag bevestigen aan je sleutels, je huisdier, je koffer of je stopt hem bijvoorbeeld in je tas. ' +
-	'De locatie van de AirTag verschijnt automatisch op de kaart in de zoek mijn- app, waar je ook al je andere Apple apparaten kan zien.',
-	13.49, 25, 'ball.com', 'Apple', 23);
-END;*/
